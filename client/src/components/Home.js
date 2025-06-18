c2927a28851489490a855945d6bf4f2e45eba16d
Aaron Reynolds <94026105+aaronreynolds421@users.noreply.github.com>
2025-06-17 20:37:49 -0700

    created the home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import Loading from "./Loading";
 
-const Loading = () => {
+const Home = () => {
+  const [fullName, setFullName] = useState("");
+  const [currentPosition, setCurrentPosition] = useState("");
+  const [currentLength, setCurrentLength] = useState(1);
+  const [currentTechnologies, setCurrentTechnologies] = useState("");
+  const [headshot, setHeadshot] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      fullName,
+      currentPosition,
+      currentLength,
+      currentTechnologies,
+      headshot,
+    });
+    setLoading(true);
+  };
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <div className="app">
-      <h1>Loading, please wait...</h1>
+      <h1>Resume Builder</h1>
+      <p>Generate a resume with ChatGPT in a few seconds</p>
+      <form
+        onSubmit={handleFormSubmit}
+        method="POST" //used for sending data to a server to create or update a resource
+        encType="multipart/form-data" //forms is crucial when dealing with file uploads
+      >
+        <label htmlFor="fullName">Enter Full Name</label>
+        <input
+          type="text"
+          required //makes this text required
+          name="fullName"
+          id="fullName"
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+        />
+
+        <div className="nestedContainer">
+          <div>
+            <label htmlFor="currentPosition">Enter Current Position</label>
+            <input
+              type="text"
+              required //makes this text required
+              name="currentPosition"
+              className="currentInput"
+              value={currentPosition}
+              onChange={(e) => setCurrentPosition(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="currentLength">How Long?</label>
+            <input
+              type="number"
+              required //makes this text required
+              name="currentLength"
+              className="currentInput"
+              value={currentLength}
+              onChange={(e) => setCurrentLength(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="currentTechnologies">Tech used</label>
+            <input
+              type="text"
+              required //makes this text required
+              name="currentTechnologies"
+              className="currentInput"
+              value={currentTechnologies}
+              onChange={(e) => setCurrentTechnologies(e.target.value)}
+            />
+          </div>
+        </div>
+        <button>Create Resume</button>
+      </form>
     </div>
   );
 };
 
-export default Loading;
+export default Home;
