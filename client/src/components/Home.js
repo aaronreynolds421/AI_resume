04b8b0df7614c55d35111563639c1a2ea38200ef
Aaron Reynolds <94026105+aaronreynolds421@users.noreply.github.com>
2025-06-21 11:17:48 -0700

    updated home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -11,14 +11,14 @@ const Home = () => {
   const [jobInfo, setJobInfo] = useState([{ name: "", position: "" }]);
 
   const handleAddJob = () =>
-    setJobInfo([...jobInfoInfo, { name: "", position: "" }]); //updates the users input by using ... to copy the the existing array and puts it into a new array
+    setJobInfo([...jobInfo, { name: "", position: "" }]); //updates the users input by using ... to copy the the existing array and puts it into a new array
   // to then append { name: "", position: "" } to the end of the new array
 
   const handleRemoveJob = (index) => {
     setJobInfo(jobInfo.filter((_, i) => i !== index)); // this function handles the removal of the job info with the press of a button.
   };
 
-  const updateJob = (e, index) => {
+  const handleUpdateJob = (e, index) => {
     const { name, value } = e.target;
     const list = [...jobInfo];
     list[index][name] = value;
@@ -93,6 +93,46 @@ const Home = () => {
             />
           </div>
         </div>
+
+        <h3>Jobs you have worked</h3>
+        <form>
+          {/*--- other UI tags --- */}
+          {jobInfo.map((Job, index) => (
+            <div className="nestedContainer" key={index}>
+              <div className="companies">
+                <label htmlFor="name">Company Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  required
+                  onChange={(e) => handleUpdateJob(e, index)}
+                />
+              </div>
+              <div className="companies">
+                <label htmlFor="position">Position Held</label>
+                <input
+                  type="text"
+                  name="position"
+                  required
+                  onChange={(e) => handleUpdateJob(e, index)}
+                />
+              </div>
+
+              <div className="btn__group">
+                {jobInfo.length - 1 === index && jobInfo.length < 4 && (
+                  <button id="addBtn" onClick={handleAddJob}>
+                    Add
+                  </button>
+                )}
+                {jobInfo.length > 1 && (
+                  <button id="deleteBtn" onClick={() => handleRemoveJob(index)}>
+                    Del
+                  </button>
+                )}
+              </div>
+            </div>
+          ))}
+        </form>
         <button>Create Resume</button>
       </form>
     </div>
